6cb7ddc488159e6e7116e51206d94d9f3fedf13a
lizl <lizhenlin@mit.com>
2022-08-14 22:22:55 +0800

    Add interface: resp

---
+++ resp/reply/consts.go
@@ -0,0 +1,63 @@
+package reply
+
+// PongReply is +PONG
+type PongReply struct{}
+
+var pongBytes = []byte("+PONG\r\n")
+
+// ToBytes marshal redis.Reply
+func (r *PongReply) ToBytes() []byte {
+	return pongBytes
+}
+
+// OkReply is +OK
+type OkReply struct{}
+
+var okBytes = []byte("+OK\r\n")
+
+// ToBytes marshal redis.Reply
+func (r *OkReply) ToBytes() []byte {
+	return okBytes
+}
+
+var theOkReply = new(OkReply)
+
+// MakeOkReply returns a ok reply
+func MakeOkReply() *OkReply {
+	return theOkReply
+}
+
+var nullBulkReply = []byte("$-1\r\n")
+
+// NullBulkReply is empty string
+type NullBulkReply struct{}
+
+// ToBytes marshal redis.Reply
+func (r *NullBulkReply) ToBytes() []byte {
+	return nullBulkReply
+}
+
+// MakeNullBulkReply creates a new NullBulkReply
+func MakeNullBulkReply() *NullBulkReply {
+	return &NullBulkReply{}
+}
+
+var emptyMultiBulkReply = []byte("*0\r\n")
+
+// EmptyMultiBulkReply is a empty list
+type EmptyMultiBulkReply struct{}
+
+// ToBytes marshal redis.Reply
+func (r *EmptyMultiBulkReply) ToBytes() []byte {
+	return emptyMultiBulkReply
+}
+
+// NoReply respond nothing, for commands like subscribe
+type NoReply struct{}
+
+var noReply = []byte("")
+
+// ToBytes marshal redis.Reply
+func (r *NoReply) ToBytes() []byte {
+	return noReply
+}
